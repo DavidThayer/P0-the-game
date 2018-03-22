3ca1c21a9167ad63e667d72ab5fa2d4c1389643c
David Thayer <david.thayer@gmail.com>
2018-03-21 17:12:29 -0700

    game logic updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 console.log('app.js loaded')
 // render game pieces
-// render game canvas
 // assign controls to each piece
+// listen for keydown to move pieces
 // trigger game winner when one piece hits finish line
 
 
@@ -9,18 +9,38 @@ console.log('app.js loaded')
 
 $(document).ready(function() { // all jQuery here
 
-  function startGame() { // set up gameboard and pieces
-    playerOnePiece = new component(30, 30, "red", 10, 120);
-    playerTwoPiece = new component(30, 30, "blue", 40, 120);
-    gameArea.start()
-  }
-  var gameArea = {
-    canvas : document.createElement('canvas'),
-    start : function () {
-      this.canvas.width = 680;
-      this.canvas.height = 480;
+var playerOnePiece = $('.player1');
+var playerTwoPiece = $('.player2');
+
+var playerOneMoves = 0;
+var playerTwoMoves = 0;
+
+$(document).on('keydown', moveRight);
+
+function moveRight(event) {
+  if (event.keyCode === 90) {
+    if (playerOneMoves < 15) {
+      $('.player1').animate({left: '+=50px'}, 100);
+      playerOneMoves++;
+    } else {
+      alert('Player one wins!')
+
     }
+
+  } else if (event.keyCode === 191) {
+      if (playerTwoMoves < 15) {
+        $('.player2').animate({left: '+=50px'}, 100);
+        playerTwoMoves++;
+      } else {
+        alert('Player two Wins!')
+
+      }
   }
+}
+
+// keep count of player 1 press and player two press
+// increment count for each keypress of player 1 and 2
+// first to 20 alerts winner and stops default action
 
 
 
